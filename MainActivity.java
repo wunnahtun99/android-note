cef9701f94af27875b35619ac241b6abffead276
Wunna Htun <wunnahtun99@gmail.com>
2018-11-03 13:10:09 +0630

    handler android

--- MainActivity.java
+++ MainActivity.java
@@ -1,62 +1,29 @@
 package com.example.wunna.timetable;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SeekBar;
-
-import java.util.ArrayList;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView timeList;
-    public void generateTime(int timeTable){
-        ArrayList<String> timeContent=new ArrayList<String>();
-        for (int i=1;i<=10;i++){
-            timeContent.add(String.valueOf(i*timeTable));
-        }
-        ArrayAdapter<String> adapeter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,timeContent);
-        timeList.setAdapter(adapeter);
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar timeSeekBar= (SeekBar) findViewById(R.id.seekBar);
-        timeList=(ListView) findViewById(R.id.timeTableList);
-        timeSeekBar.setMax(20);
-        timeSeekBar.setProgress(10);
-        timeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+       final Handler handle=new Handler();
+        Runnable run=new Runnable() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int min=1;
-                int timeTable;
-                if (progress<min){
-                    timeTable=min;
-                }
-                else {
-                    timeTable=progress;
-                }
-           //     Log.i("Seek Bar value", String.valueOf(timeTable));
-            generateTime(progress);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            public void run() {
+                Log.i("delay","1s");
+                handle.postDelayed(this,1000);
 
             }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        generateTime(10);
-
+        };
+        handle.post(run);
 
     }
 }
