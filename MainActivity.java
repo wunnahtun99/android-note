98b961313f1cd2636df1c37a5c65bf695e52da89
Wunna Htun <wunnahtun99@gmail.com>
2018-11-09 15:29:36 +0630

    Json data processing
    Weather json data

--- MainActivity.java
+++ MainActivity.java
@@ -1,16 +1,17 @@
-package com.example.wunna.web;
+package com.example.wunna.jsondemo;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,57 +19,74 @@ import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button download;
-    ImageView downloadImage;
-
-    public class ImageDownloader extends AsyncTask<String,Void,Bitmap>{
+    public class DownloadTask extends AsyncTask<String,Void,String>{
 
         @Override
-        protected Bitmap doInBackground(String... urls) {
-
-            try {
-                URL url=new URL(urls[0]);
-                HttpURLConnection connection=(HttpURLConnection)url.openConnection();
-                connection.connect();
-                InputStream inputStream=connection.getInputStream();
-                Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
-                return bitmap;
-
+        protected String doInBackground(String... strings) {
+            String result="";
+            URL url;
+            HttpURLConnection urlConnection=null;
+            try
+            {
+                url=new URL(strings[0]);
+                urlConnection=(HttpURLConnection) url.openConnection();
+                InputStream inputStream=urlConnection.getInputStream();
+                InputStreamReader reader=new InputStreamReader(inputStream);
+                int data=reader.read();
+                while (data!=-1){
+                    char current=(char) data;
+                    result+=current;
+                    data=reader.read();
+                }
+                return result;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
+                return "fail";
             } catch (IOException e) {
                 e.printStackTrace();
+                return "fail";
             }
-            return null;
-        }
-    }
 
 
-    public void downloadImage(View view){
-        //URL https://i.pinimg.com/originals/c7/43/a5/c743a5c03efdf60b1e57f22087a288ea.jpg
 
-        ImageDownloader task=new ImageDownloader();
-        Bitmap myImage;
-        try {
-            myImage=task.execute("https://i.pinimg.com/originals/c7/43/a5/c743a5c03efdf60b1e57f22087a288ea.jpg").get();
-            downloadImage.setImageBitmap(myImage);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
         }
 
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            try {
+                JSONObject jsonObject=new JSONObject(result);
+                String weatherInf=jsonObject.getString("weather");
+                JSONArray jsonArray=new JSONArray(weatherInf);
+                for (int i=0;i<jsonArray.length();i++){
+                    JSONObject jsonPart=jsonArray.getJSONObject(i);
+                    Log.i("main ", jsonPart.getString("main"));
+                    Log.i("description ", jsonPart.getString("description"));
 
+
+                }
+                Log.i("weather",weatherInf);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.i("Web content",result);
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        download= (Button) findViewById(R.id.button);
-        downloadImage= (ImageView) findViewById(R.id.imageView);
-
-
-
+        DownloadTask task=new DownloadTask();
+        String result=null;
+        try {
+            result=task.execute("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        Log.i("content",result);
     }
 }
